4c77577df439b22ac83c3dd5ac796f0d420e20ca
ryuunderground <jaeking00001@gmail.com>
2024-12-24 18:17:18 +0900

    string interpolation

--- main.dart
+++ main.dart
@@ -1,24 +1,6 @@
 void main() {
-  //Basic types
-  String name = "John";
-  bool isAlive = true;
-  double height = 1.8;
-  int age = 20;
-  //List
-  var character = [name, isAlive, height];
-  print(character);
-  print(character[0]);
-  print(character.length);
-  print(character.first);
-  print(character.last);
-  print(character.reversed);
-  print(character.isEmpty);
-  print(character.isNotEmpty);
-  character.add(age);
-  print(character.first);
-  print(character.last);
-
-  var giveMeSix = true;
-  List<int> numbers = [1, 2, 3, 4, 5, if (giveMeSix) 6];
-  print(numbers);
+  var name = "John";
+  var age = 25;
+  var greeting = "Hello, $name. Good to see you. u r ${age + 2} years old";
+  print(greeting);
 }
