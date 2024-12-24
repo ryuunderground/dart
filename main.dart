bc32d9090cf1174bc8771ef174bac01adb3d6ae7
ryuunderground <jaeking00001@gmail.com>
2024-12-24 18:11:20 +0900

    basic types and list

--- main.dart
+++ main.dart
@@ -1,4 +1,24 @@
 void main() {
-  const API_KEY = "1223123";
-  //variable sould be known at compile time
+  //Basic types
+  String name = "John";
+  bool isAlive = true;
+  double height = 1.8;
+  int age = 20;
+  //List
+  var character = [name, isAlive, height];
+  print(character);
+  print(character[0]);
+  print(character.length);
+  print(character.first);
+  print(character.last);
+  print(character.reversed);
+  print(character.isEmpty);
+  print(character.isNotEmpty);
+  character.add(age);
+  print(character.first);
+  print(character.last);
+
+  var giveMeSix = true;
+  List<int> numbers = [1, 2, 3, 4, 5, if (giveMeSix) 6];
+  print(numbers);
 }
