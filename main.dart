db44569947e7d1b79a8ad168c03c513a76b65ccf
ryuunderground <jaeking00001@gmail.com>
2024-12-27 02:58:54 +0900

    Optional positional parameters

--- main.dart
+++ main.dart
@@ -1,28 +1,8 @@
-String sayHello(String name) => 'Hello, $name!';
-
-/* 
-// Same as above
-String sayHello(String name) {
-return('Hello, $name!');
-} */
-
-/* String hBD(String name, int age, String location) =>
-    'Happy Birthday, $name! You are $age years old! You live in $location.';
-*/
-
-String hBD({
-  required String name,
-  required int age,
-  required String location}) {
+String hBD(String name, int age, [String? location = 'unknown']) {
   return ('Happy Birthday, $name! You are $age years old! You live in $location.');
 }
 
 void main() {
-  print(sayHello('John'));
-  /* print(hBD('John', 20, 'New York')); */
-  print(hBD(
-    name: 'John',
-    age: 20,
-    ,
-    ));
+  print(hBD('John', 20, 'New York'));
+  print(hBD('Amy', 22));
 }
