0c07be05735500262ff527a20d4670e42571e90d
ryuunderground <jaeking00001@gmail.com>
2024-12-30 04:38:43 +0900

    abstract class, now it is not populary used.

--- main.dart
+++ main.dart
@@ -1,14 +1,28 @@
+abstract class Human {
+  void walk();
+}
+
 enum Team { Good, Evil, Wicked, Chaos }
 
-class Players {
+class Players extends Human {
   String name;
   int level;
   Team team;
 
   Players({required this.name, required this.level, required this.team});
 
+  void walk() {
+    print("I'm walking");
+  }
+
   void selfIntroduction() {
     print("Hello, I'm $name. I'm a $level level player in $team.");
+  }
+}
+
+class Joney extends Human {
+  void walk() {
+    print("I'm Joney walker");
   }
 }
 
@@ -18,5 +32,6 @@ void main() {
     ..level = 28
     ..team = Team.Chaos
     ..selfIntroduction();
-  print(Ryu);
+  var Black = Joney();
+  Black.walk();
 }
